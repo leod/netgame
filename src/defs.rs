34ff1a99c26d9b780ca7d61365a6db2fa69754ce
leod <subtle.frustration@proton.me>
2018-10-06 20:19:27 +0200

    Initial commit

---
+++ src/defs.rs
@@ -0,0 +1,5 @@
+/// A unique id assigned by the server to a client.
+pub struct ClientId(u32);
+
+/// A unique id assigned by the server to a player.
+pub struct PlayerId(u32);
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+extern crate specs;
+#[macro_use]
+extern crate specs_derive;
+
+mod defs;
+mod entity;
+
+pub use defs::*;
